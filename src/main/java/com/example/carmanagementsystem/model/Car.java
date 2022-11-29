5f1816a280ed9e6c8be575bee96b0a6c76f905fe
mohammad-saeid-nafar <mohammad.nafar@mail.mcgill.ca>
2022-11-29 17:20:10 -0500

    Creating and Reading Cars Done
    Can successfully add a car to database and also get all cars within database

---
+++ src/main/java/com/example/carmanagementsystem/model/Car.java
@@ -0,0 +1,43 @@
+package com.example.carmanagementsystem.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Car {
+
+    @jakarta.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int Id;
+    private String modelName;
+    private int cost;
+
+    public Car() {
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+}
